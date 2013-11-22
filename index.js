937d3e713694eb1bef2104d164ce8e67d12973d2
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2013-11-22 19:58:09 +0100

    renamed plugins to compnents, added routes, made express server a component

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
 #!/usr/bin/env node
 
-var myapp = require('./lib/broadwayapp');
+var app = require('./lib/broadwayapp');
 
-console.log('on');
-myapp.on('ready', start_server);
-
-function start_server() {
-    myapp.log.info('starting server');
-    
-    var express = require('express');
-    var app = express();
-    
-    app.get('/', serve_root_url);
-    app.listen(3000, show_started_message);
-}
-
-function serve_root_url(req, res) {
-    myapp.emit('ping');
-    res.send(myapp.info);
-}
-
-function show_started_message() {
-    myapp.log.info('listening.');
-}
+app.on('ready',
+    function () { app.log.info('starting up'); app.server.start() }
+);
